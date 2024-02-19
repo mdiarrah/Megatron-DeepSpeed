750599bd5a08c81153e501293d4dfb5ce8a77c8e
Mamoutou <mamoutou.diarra@hivenet.com>
2024-02-19 13:50:54 +0000

    add hive bert finetuning

---
+++ hive/finetune_bert.py
@@ -0,0 +1,38 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import AutoModel, Trainer, TrainingArguments
+from transformers import AutoModelForSequenceClassification
+import numpy as np
+from datasets import load_metric
+
+metric = load_metric("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+
+raw_datasets = load_dataset("imdb")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+full_train_dataset = tokenized_datasets["train"]
+full_eval_dataset = tokenized_datasets["test"]
+training_args = TrainingArguments("test_trainer",evaluation_strategy="epoch", deepspeed="./ds_config_zero3.json")
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+
+trainer = Trainer(
+    model=model, 
+    args=training_args, 
+    train_dataset=small_train_dataset, 
+    eval_dataset=small_eval_dataset,
+    compute_metrics=compute_metrics,
+)
+trainer.evaluate()
+trainer.train()
