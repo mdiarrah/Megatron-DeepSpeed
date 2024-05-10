a843bfac56cdacc72ca0ea2cd7cd90b3c27e056e
Mamoutou <mamoutou.diarra@hivenet.com>
2024-05-10 15:05:11 +0200

    remove full path

--- examples_deepspeed/finetune_hf_llama/finetune_hf_llama_default.sh
+++ examples_deepspeed/finetune_hf_llama/finetune_hf_llama_default.sh
@@ -53,12 +53,12 @@ cat <<EOT > $DS_CONFIG
 EOT
 
 #--origin-hf-ckpt-dir $HF_LLAMA_REPO \
-covert_args="deepspeed --num_nodes $NODES \ /home/deepspeed/Megatron-DeepSpeed/tools/hf2megads_weight_converter.py \
+covert_args="deepspeed tools/hf2megads_weight_converter.py \
 --hf-ckpt-num-shards 8 \
 --origin-hf-ckpt-dir $HF_LLAMA_PATH \
 --save $MEGA_DS_LLAMA_PATH"
 
-finetune_args="deepspeed --num_nodes $NODES \ /home/deepspeed/Megatron-DeepSpeed/finetune_llama.py \
+finetune_args="deepspeed finetune_llama.py \
 --load $MEGA_DS_LLAMA_PATH"
 
 comm_args="--tensor-model-parallel-size $TP \
